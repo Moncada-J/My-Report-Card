05f09b2bceef6acacb4cfd033b97bc5257b3e664
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-06-14 22:05:59 -0700

    updates navbar and adds custom made banner for /students page

--- src/pages/Students/StudentsPage.jsx
+++ src/pages/Students/StudentsPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+// import Video from './welcomebanner';
 // import * as usersService from '../../utilities/users-service';
 import Student from '../../Components/Student/Student';
+import Banner from './welcome.gif';
+// import Video from '../../Components/BannerVideo/BannerVideo';
 
 function StudentsPage({students, handleDeleteStudent}) {
 	const s = students.map(student => 
@@ -12,6 +15,8 @@ function StudentsPage({students, handleDeleteStudent}) {
 		);
 	return (
 	<>
+	{/* <Video /> */}
+	<img className="banner" src={Banner} alt="banner" />
 	<h1>Students in the Classroom</h1>
 		<div>
 			{s}
